330c35ac1c647ecec254a6e1aebc309325989fa7
Dzion Tucker <36012047+TuckerIBarleyEvenknowHer@users.noreply.github.com>
2018-05-22 04:03:16 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const data = [
+    {  
+         "id": 1,
+        "first_name": "Maddie",
+        "last_name": "St Ange",
+        "email": "mstange0@domainmarket.com",
+        "company": "Photofeed",
+        "phone": "941-586-0450"
+    },
+    {   "id": 2,
+        "first_name": "Bette",
+        "last_name": "Kubiak",
+        "email": "bkubiak1@bigcartel.com",
+        "company": "Zoonoodle",
+        "phone": "186-745-2295"
+    },
+    {   "id": 3,
+        "first_name": "Morna",
+        "last_name": "Wyllcock",
+        "email": "mwyllcock2@simplemachines.org",
+        "company": "Realcube",
+        "phone": "915-230-8763"
+    },
+    {   "id": 4,
+        "first_name": "Kaylyn",
+        "last_name": "Annis",
+        "email": "kannis3@archive.org",
+        "company": "Edgeify",
+        "phone": "303-881-5186"
+    },
+    {   "id": 5, 
+        "first_name": "Emmaline",
+        "last_name": "Shildrake",
+        "email": "eshildrake4@goo.gl",
+        "company": "Zazio",
+        "phone": "953-123-8363"
+    }
+]
+module.exports = data;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import routes from "./src/routes/routes";
 
 const app = express();
 const PORT = 3000;
+let contacts = require('./data');  // grabing from data 
 
 //mongo connection
 mongoose.connect("mongodb://localhost/project2");
@@ -15,12 +16,72 @@ app.use(bodyParser.json()); //parse incoming requests with json payloads
 
 routes(app);
 
+
+
+
 //prints stuff to homepage
 app.get("/", (req, res) => {
-  res.send(`Node and express server running on port: ${PORT}`);
+  res.send(contacts);
+  
+});
+//finding a particular person by there id number 
+app.get('/:id', (req,res)=> {
+  const requestID = req.params.id;
+
+  let contact = contacts.filter(contact => {
+    return contact.id == requestID;
+  })
+  res.json(contact[0]);
+})
+
+//adding new contacts 
+app.post('/', (req,res) => {
+  const contact = {
+    id: contacts.length + 1,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    company: req.body.company,
+    phone : req.body.phone,
+
+  }
+contacts.push(contact);
+res.json(contact);
+});
+//for when you want to update your infomation 
+app.put('/:id', (req,res) => {
+  const requestID = req.params.id;
+
+  let contact = contacts.filter(contact =>{
+    return contact.id == requestID;
+  })[0];
+
+const index = contacts.indexOf(contact);
+
+const keys = Object.keys(req.body);
+
+keys.forEach(key => {
+  contact[key] = req.body[key];
+});
+contacts[index] = contact;
+
+  res.json(contacts[index]);
+});
+
+app.delete('/:id', (req,res) => {
+const requestID = req.params.id;
+let contact = contacts.filter(contact => {
+  return contact.id == requestID;
+})[0];
+const index = contacts.indexOf(contact);
+
+contacts.splice(index, 1);
+
+res.json({message: `user  ${requestID}  deleted`});
 });
 
 //server is listening on this port
 app.listen(PORT, () => {
   console.log(`the server is up and running on localhost:${PORT}`);
 });
+
